97f5371a882cc2816c02eae3d4179de4984ae28f
Pierre Hugo <pierrekin@househugo.com>
2026-08-07 00:42:34 +0200

    fix: reject deriving event for generic types

--- crates/rillet-macros/src/event.rs
+++ crates/rillet-macros/src/event.rs
@@ -2,10 +2,17 @@
 
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{DeriveInput, Result};
+use syn::{DeriveInput, Error, Result};
 
 /// Expands `#[derive(Event)]`.
 pub fn expand(input: DeriveInput) -> Result<TokenStream> {
+    if !input.generics.params.is_empty() {
+        return Err(Error::new_spanned(
+            &input.generics,
+            "Event cannot be derived for generic types",
+        ));
+    }
+
     let name = &input.ident;
 
     Ok(quote! {
